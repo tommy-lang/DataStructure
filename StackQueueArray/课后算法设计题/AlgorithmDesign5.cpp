5fd95fa03baa8945423cd64c2943459a0082137b
tommy-lang <628304hans@gmail.com>
2025-03-19 11:10:07 +0800

    C，C++

--- StackQueueArray/课后算法设计题/AlgorithmDesign5.cpp
+++ StackQueueArray/课后算法设计题/AlgorithmDesign5.cpp
@@ -1,3 +1,21 @@
-//
-// Created by 雨墨 on 2025/3/19.
-//
+#include <iostream>
+#include <string>
+
+#define MAXSIZE 100
+
+typedef struct
+{
+    char* base;
+    char* top;
+    int StackSize;
+    int CurrentSize;
+} SqStack;
+
+// 初始化栈
+void InitStack(SqStack& S)
+{
+    S.base = new char[MAXSIZE];
+    S.top = S.base;
+    S.StackSize = MAXSIZE;
+    S.CurrentSize=0;
+}
